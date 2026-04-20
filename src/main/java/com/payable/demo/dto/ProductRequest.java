82ddc5bb8c32c6c70bcc5dd4d1f733dd18e5dee0
Anjana Rasadari Isurundi <anjanarasadari21@gmail.com>
2026-04-21 04:13:34 +0530

    Initial commit: src/main/java/com/payable/demo/dto/ProductRequest.java

---
+++ src/main/java/com/payable/demo/dto/ProductRequest.java
@@ -0,0 +1,34 @@
+package com.payable.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductRequest {
+    @NotBlank(message = "SKU is required")
+    private String sku;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be positive")
+    private BigDecimal price;
+
+    @NotBlank(message = "Currency is required")
+    private String currency;
+
+    private Boolean isActive;
+}
