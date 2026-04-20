274fdc8fa29beade6032543226504fb4a3cf287a
Anjana Rasadari Isurundi <anjanarasadari21@gmail.com>
2026-04-21 04:14:03 +0530

    Initial commit: src/main/java/com/payable/demo/security/CustomUserDetails.java

---
+++ src/main/java/com/payable/demo/security/CustomUserDetails.java
@@ -0,0 +1,21 @@
+package com.payable.demo.security;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+public class CustomUserDetails extends User {
+    private final Long userId;
+    private final String propertyId;
+
+    public CustomUserDetails(String username, String password, boolean enabled, boolean accountNonExpired,
+                             boolean credentialsNonExpired, boolean accountNonLocked,
+                             Collection<? extends GrantedAuthority> authorities, Long userId, String propertyId) {
+        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        this.userId = userId;
+        this.propertyId = propertyId;
+    }
+}
