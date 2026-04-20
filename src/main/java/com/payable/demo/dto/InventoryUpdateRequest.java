c472ec0a308229ad7c57d5150a81ffdb32a57b57
Anjana Rasadari Isurundi <anjanarasadari21@gmail.com>
2026-04-21 04:13:27 +0530

    Initial commit: src/main/java/com/payable/demo/dto/InventoryUpdateRequest.java

---
+++ src/main/java/com/payable/demo/dto/InventoryUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.payable.demo.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class InventoryUpdateRequest {
+    @NotNull(message = "Product ID is required")
+    private Long productId;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity cannot be negative")
+    private Integer quantity;
+}
